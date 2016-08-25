8f1c33dc2e7021b52457a0c120c7507ff2c53937
Gaber Setina <gaber.setina@netural.com>
2016-08-25 12:55:02 +0200

    init commit

---
+++ app/movie.js
@@ -0,0 +1,50 @@
+'use strict';
+
+let Wit = null;
+let interactive = null;
+try {
+  // if running from repo
+  Wit = require('../').Wit;
+  interactive = require('../').interactive;
+} catch (e) {
+  Wit = require('node-wit').Wit;
+  interactive = require('node-wit').interactive;
+}
+
+const accessToken = (() => {
+  if (process.argv.length !== 3) {
+    console.log('usage: node examples/basic.js <wit-access-token>');
+    process.exit(1);
+  }
+  return process.argv[2];
+})();
+
+
+const allJokes = {
+  chuck: [
+    'Chuck Norris counted to infinity - twice.',
+    'Death once had a near-Chuck Norris experience.',
+  ],
+  tech: [
+    'Did you hear about the two antennas that got married? The ceremony was long and boring, but the reception was great!',
+    'Why do geeks mistake Halloween and Christmas? Because Oct 31 === Dec 25.',
+  ],
+  default: [
+    'Why was the Math book sad? Because it had so many problems.',
+  ],
+};
+
+const actions = {
+  send(request, response) {
+    const {sessionId, context, entities} = request;
+    const {text, quickreplies} = response;
+    return new Promise(function(resolve, reject) {
+      console.log('user said...', request.text);
+      console.log('sending...', JSON.stringify(response));
+      return resolve();
+    });
+  },
+};
+
+const client = new Wit({accessToken, actions});
+interactive(client);
